6375424f61e7aaa1683580a5c641732c21394e35
mrk8111 <jmate90@hotmail.com>
2024-09-17 21:11:08 -0500

    Create ListaEnemigo.h

---
+++ TParcial/TParcial/ListaEnemigo.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Enemigo.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+using namespace System;
+
+class ListaEnemigo {
+protected:
+	vector<Enemigo*>arregloEnemigo;
+public:
+	ListaEnemigo();
+	void agregarEnemigo(Enemigo* nuevo);
+	Enemigo* obtenerEnemigo(int indice);
+	int tamaño();
+	vector<Enemigo*> retornar();
+};
+
+ListaEnemigo::ListaEnemigo() {
+	arregloEnemigo = vector<Enemigo*>();
+}
+
+void ListaEnemigo::agregarEnemigo(Enemigo* nuevo) {
+	arregloEnemigo.push_back(nuevo);
+}
+
+Enemigo* ListaEnemigo::obtenerEnemigo(int indice) {
+	return arregloEnemigo.at(indice);
+}
+
+int ListaEnemigo::tamaño() {
+	return arregloEnemigo.size();
+}
+
+vector<Enemigo*> ListaEnemigo::retornar() {
+	return arregloEnemigo;
+}
