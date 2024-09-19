bd45c8cf6442bcf6c284eafc6a3332bfb3b78dff
mrk8111 <jmate90@hotmail.com>
2024-09-19 15:50:46 -0500

    Update Personaje.h

--- TParcial/TParcial/Personaje.h
+++ TParcial/TParcial/Personaje.h
@@ -10,29 +10,31 @@ private:
 	int ancho, alto;
 public:
 	Personaje();
-	Personaje(int _px, int _py, int _pdx, int _pdy, int pancho, int palto);
+	Personaje(int _px, int _py);
 	~Personaje();
 	void borrar();
-	void mover();
+	void mover(char tecla);
 	void dibujar();
-	void direccion(char tecla);
+
+	int getPX();
+	int getPY();
 };
 Personaje::Personaje() {
-	this->px = 90;
-	this->py = 38;
+	this->px = 0;
+	this->py = 0;
 	this->pdx = 0;
 	this->pdy = 0;
 	this->ancho = Console::WindowWidth;
 	this->alto = Console::WindowHeight;
 }
 
-Personaje::Personaje(int _px, int _py, int _pdx, int _pdy, int pancho, int palto) {
+Personaje::Personaje(int _px, int _py) {
 	this->px = _px;
 	this->py = _py;
-	this->pdx = _pdx;
-	this->pdy = _pdy;
-	this->ancho = pancho;
-	this->alto = palto;
+	this->pdx = 0;
+	this->pdy = 0;
+	this->ancho = 0;
+	this->alto = 0;
 }
 Personaje::~Personaje() {}
 
@@ -40,30 +42,29 @@ void Personaje::borrar() {
 	Console::SetCursorPosition(px, py); cout << "     ";
 }
 
-void Personaje::mover() {
-	if (px < 0) px = 0;
-	if (px >= ancho) px = ancho - 1;
-	if (py < 0) py = 0;
-	if (py >= alto) py = alto - 1;
-	px += pdx;
-	py += pdy;
+void Personaje::mover(char tecla) {
+	if (tecla == 'W' && py > 0) {
+		py--;
+	}
+	if (tecla == 'S' && py < 39) {
+		py++;
+	}
+	if (tecla == 'D' && px < 119) {
+		px++;
+	}
+	if (tecla == 'A' && px > 0) {
+		px--;
+	}
 }
 
 void Personaje::dibujar() {
 	Console::SetCursorPosition(px, py); cout << "@";
 }
 
-void Personaje::direccion(char tecla) {
-	if (tecla == 'W') { //arriba
-		pdy = -1, pdx = 0; //para q vaya recto en una sola direccion
-	}
-	else if (tecla == 'S') { //abajo
-		pdy = 1, pdx = 0;
-	}
-	else if (tecla == 'D') { //derecha
-		pdx = 2, pdy = 0;
-	}
-	else if (tecla == 'A') { //izquierda
-		pdx = -2, pdy = 0;
-	}
+int Personaje::getPX() {
+	return this->px;
+}
+
+int Personaje::getPY() {
+	return this->py;
 }
