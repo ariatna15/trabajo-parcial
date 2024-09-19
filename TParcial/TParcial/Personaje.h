0ae72f246248f6519f6c4b8fef501f109fa254b9
mrk8111 <jmate90@hotmail.com>
2024-09-19 00:14:07 -0500

    Update Personaje.h

--- TParcial/TParcial/Personaje.h
+++ TParcial/TParcial/Personaje.h
@@ -5,12 +5,12 @@ using namespace std;
 
 class Personaje {
 private:
-	int x, y;
-	int dx, dy;
+	int px, py;
+	int pdx, pdy;
 	int ancho, alto;
 public:
 	Personaje();
-	Personaje(int px, int py, int pdx, int pdy, int pancho, int palto);
+	Personaje(int _px, int _py, int _pdx, int _pdy, int pancho, int palto);
 	~Personaje();
 	void borrar();
 	void mover();
@@ -18,52 +18,52 @@ public:
 	void direccion(char tecla);
 };
 Personaje::Personaje() {
-	this->x = 90;
-	this->y = 38;
-	this->dx = 0;
-	this->dy = 0;
+	this->px = 90;
+	this->py = 38;
+	this->pdx = 0;
+	this->pdy = 0;
 	this->ancho = Console::WindowWidth;
 	this->alto = Console::WindowHeight;
 }
 
-Personaje::Personaje(int px, int py, int pdx, int pdy, int pancho, int palto) {
-	this->x = px;
-	this->y = py;
-	this->dx = pdx;
-	this->dy = pdy;
+Personaje::Personaje(int _px, int _py, int _pdx, int _pdy, int pancho, int palto) {
+	this->px = _px;
+	this->py = _py;
+	this->pdx = _pdx;
+	this->pdy = _pdy;
 	this->ancho = pancho;
 	this->alto = palto;
 }
 Personaje::~Personaje() {}
 
 void Personaje::borrar() {
-	Console::SetCursorPosition(x, y); cout << "     ";
+	Console::SetCursorPosition(px, py); cout << "     ";
 }
 
 void Personaje::mover() {
-	if (x < 0) x = 0;
-	if (x >= ancho) x = ancho - 1;
-	if (y < 0) y = 0;
-	if (y >= alto) y = alto - 1;
-	x += dx;
-	y += dy;
+	if (px < 0) px = 0;
+	if (px >= ancho) px = ancho - 1;
+	if (py < 0) py = 0;
+	if (py >= alto) py = alto - 1;
+	px += pdx;
+	py += pdy;
 }
 
 void Personaje::dibujar() {
-	Console::SetCursorPosition(x, y); cout << "@";
+	Console::SetCursorPosition(px, py); cout << "@";
 }
 
 void Personaje::direccion(char tecla) {
 	if (tecla == 'W') { //arriba
-		dy = -1, dx = 0; //para q vaya recto en una sola direccion
+		pdy = -1, pdx = 0; //para q vaya recto en una sola direccion
 	}
 	else if (tecla == 'S') { //abajo
-		dy = 1, dx = 0;
+		pdy = 1, pdx = 0;
 	}
 	else if (tecla == 'D') { //derecha
-		dx = 2, dy = 0;
+		pdx = 2, pdy = 0;
 	}
 	else if (tecla == 'A') { //izquierda
-		dx = -2, dy = 0;
+		pdx = -2, pdy = 0;
 	}
 }
