ce81b339dd3c120ab466b705a38a001465f1f91e
mrk8111 <jmate90@hotmail.com>
2024-09-19 01:12:52 -0500

    Update Source.cpp

--- TParcial/TParcial/Source.cpp
+++ TParcial/TParcial/Source.cpp
@@ -1,11 +1,24 @@
+#include <iostream>
+#include "Juego.h"
 #include "Personaje.h"
 #include "conio.h"
+using namespace std;
+using namespace System;
+
+int generarAleatorio(int max, int min) {
+	return min + rand() % (max - min + 1);
+}
+
 
 int main() {
 	Console::SetWindowSize(180, 40);
 	Console::CursorVisible = false;
 	Personaje* Objpersonaje;
 	Objpersonaje = new Personaje();
+
+	int aguaTotal = generarAleatorio(4,9);
+	int semillaTotal = generarAleatorio(4,9);
+	int residuoTotal = generarAleatorio(4,9);
 
 	char tecla;
 	while (1) {
